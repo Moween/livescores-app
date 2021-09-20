d88c31d4d5f4a578d96f8afc2e692cc608e39f8b
moween <moween1998@gmail.com>
2021-09-20 11:31:20 +0100

    Style soccer icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import SportsSoccerIcon from '@material-ui/icons/SportsSoccer';
 import { makeStyles } from '@material-ui/styles';
 
 const useStyles = makeStyles({
   brandLogo: {
     color: 'white',
-    padding: 10
+    padding: 10,
+    paddingLeft: 0
+  },
+  logoContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  soccerIcon: {
+    color: 'white'
   }
 })
 
 const Header = () => {
-  const { brandLogo } = useStyles();
+  const { brandLogo, logoContainer, soccerIcon } = useStyles();
   return (
     <header>
-      <Container>
-        <Typography className={brandLogo} variant="h4" component="h1">
+      <Container className={logoContainer}>
+        <SportsSoccerIcon className={soccerIcon} />
+        <Typography 
+          className={brandLogo} 
+          variant="h4" component="h1" 
+        >
           Live
           <Typography variant="h4" component="span" color="secondary">
           Score
