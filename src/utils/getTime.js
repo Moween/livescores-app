72ac189f53d3682675c49b0ec5a76c90b3f40468
moween <moween1998@gmail.com>
2021-09-21 11:18:22 +0100

    Refactor code

--- src/utils/getTime.js
+++ src/utils/getTime.js
@@ -1,13 +1,12 @@
 const getTime = (date) => {
   let jsonDate = { ...date };
   jsonDate = JSON.stringify(jsonDate);
-  let obj = JSON.parse(jsonDate);
-  obj = new Date(obj.date).toUTCString();
-  let time = new Date(obj)
+  let time = JSON.parse(jsonDate);
+  time = new Date(time.date).toUTCString();
+  return new Date(time)
     .toLocaleTimeString()
     .toString()
     .slice(0, 5);
-  return time;
 };
 
 export default getTime;
