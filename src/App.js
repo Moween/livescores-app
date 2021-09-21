ff63847dcc8d27026c49f45b04ab5d5d55f3f4c8
moween <moween1998@gmail.com>
2021-09-21 11:17:18 +0100

    Set breakpoints

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import './css/App.css';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  breakpoints: {
+    values:{
+      'xs': 0,
+      'sm': 375,
+      'md': 768,
+      'lg': 960,
+      'xl': 1140,
+    },
+    unit: 'px'
+  },
+  palette: {
+    secondary: {
+      main: '#42b72a'
+    }
+  }
+});
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
       <Main />
-    </>
+    </ThemeProvider>
   );
 }
 
