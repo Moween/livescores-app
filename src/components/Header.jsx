cb953f464366c0ff2e8083cb88f2fc827ebd328a
moween <moween1998@gmail.com>
2021-10-16 08:58:59 +0100

    Import useStyles from styles script

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,22 +2,7 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import SportsSoccerIcon from '@material-ui/icons/SportsSoccer';
-import { makeStyles } from '@material-ui/styles';
-
-const useStyles = makeStyles({
-  brandLogo: {
-    color: 'white',
-    padding: 10,
-    paddingLeft: 0
-  },
-  logoContainer: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  soccerIcon: {
-    color: 'white'
-  }
-})
+import { useStyles } from '../utils/styles';
 
 const Header = () => {
   const { brandLogo, logoContainer, soccerIcon } = useStyles();
